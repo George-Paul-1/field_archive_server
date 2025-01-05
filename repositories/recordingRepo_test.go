1b8b7629fbfb8c685f667569f5959e2a22057bf7
George-Paul-1 <george.paul5236@gmail.com>
2025-01-05 23:45:45 +0000

    Test written for insert on recording repository

--- repositories/recordingRepo_test.go
+++ repositories/recordingRepo_test.go
@@ -1,56 +1,59 @@
 package repositories
 
-// import (
-// 	"context"
-// 	"field_archive/server/entities"
-// 	"testing"
-// 	"time"
+import (
+	"context"
+	"errors"
+	"field_archive/server/entities"
+	"testing"
+	"time"
 
-// 	"github.com/pashagolub/pgxmock"
-// )
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
+	"github.com/stretchr/testify/assert"
+)
 
-// func TestInsert(t *testing.T) {
-// 	mock, err := pgxmock.NewConn()
-// 	if err != nil {
-// 		t.Fatalf("failed to create pgxMock", err)
-// 	}
-// 	defer mock.Close(context.Background())
+type MockDatabase struct {
+	mockExec     func(ctx context.Context, query string, args ...any) (pgconn.CommandTag, error)
+	mockQueryRow func(ctx context.Context, query string, args ...any) pgx.Row
+}
 
-// 	recording := entities.Recording{
-// 		Title:         "Test Title",
-// 		AudioLocation: "test/audio/location.mp3",
-// 		DateUploaded:  time.Now(),
-// 		RecordingDate: time.Now(),
-// 		LocationID:    1,
-// 		Duration:      120,
-// 		Format:        "mp3",
-// 		Description:   "Test description",
-// 		Equipment:     "Test equipment",
-// 		Size:          2048,
-// 		Channels:      2,
-// 		License:       "Creative Commons",
-// 	}
+func (m *MockDatabase) Exec(ctx context.Context, query string, args ...any) (pgconn.CommandTag, error) {
+	return m.mockExec(ctx, query, args...)
+}
+func (m *MockDatabase) QueryRow(ctx context.Context, query string, args ...any) pgx.Row {
+	return m.mockQueryRow(ctx, query, args...)
+}
 
-// 	query := `INSERT INTO recordings` +
-// 	`\(id, title, audio_location, artwork_location, date_uploaded, recording_date, location_id,` +
-// 	` duration, format, description, equipment, file_size, channels, license\) ` +
-// 	`VALUES` +
-// 	`\(@title, @audio_location, @date_uploaded, @recording_date, @location_id, @duration, @format, @description, @equipment, @file_Size, @channels, @license\)`
-// 	mock.ExpectExec(query).
-// 		WithArgs(
-// 			recording.Title,
-// 			recording.AudioLocation,
-// 			recording.DateUploaded,
-// 			recording.RecordingDate,
-// 			recording.LocationID,
-// 			recording.Duration,
-// 			recording.Format,
-// 			recording.Description,
-// 			recording.Equipment,
-// 			recording.Size,
-// 			recording.Channels,
-// 			recording.License,
-// 		).
-// 	WillReturnResult(pgxmock.NewResult("INSERT", 1))
-// 	repo := &RecordingRepoImplement{conn: mock,}
-// }
+func TestInsert(t *testing.T) {
+	check := `INSERT INTO recordings` +
+		`(id, title, audio_location, artwork_location, date_uploaded, recording_date, location_id, ` +
+		`duration, format, description, equipment, file_size, channels, license) ` +
+		`VALUES ` +
+		`(@title, @audio_location, @date_uploaded, @recording_date, @location_id, @duration, @format, @description, @equipment, @file_Size, @channels, @license)`
+
+	mockDB := &MockDatabase{
+		mockExec: func(ctx context.Context, query string, args ...any) (pgconn.CommandTag, error) {
+			if check == query {
+				return pgconn.CommandTag{}, nil
+			}
+			return pgconn.CommandTag{}, errors.New("query mismatch")
+		},
+	}
+	repo := &RecordingRepoImplement{conn: mockDB}
+	recording := entities.Recording{
+		Title:         "Test Title",
+		AudioLocation: "test/audio/location.mp3",
+		DateUploaded:  nil,
+		RecordingDate: time.Now(),
+		LocationID:    1,
+		Duration:      120,
+		Format:        "mp3",
+		Description:   "Test description",
+		Equipment:     "Test equipment",
+		Size:          2048,
+		Channels:      "2",
+		License:       "Creative Commons",
+	}
+	err := repo.Insert(recording, context.Background())
+	assert.NoError(t, err)
+}
