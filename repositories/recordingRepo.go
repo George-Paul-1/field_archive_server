ccd4a13ae7c606fa9cd796e71485789fb212a504
George-Paul-1 <george.paul5236@gmail.com>
2025-01-06 21:14:11 +0000

    Added Update method to recordingRepo

--- repositories/recordingRepo.go
+++ repositories/recordingRepo.go
@@ -86,6 +86,33 @@ func (r *RecordingRepoImplement) GetRowByID(id int, ctx context.Context) (entiti
 }
 
 func (r *RecordingRepoImplement) Update(recording entities.Recording, ctx context.Context) error {
+	fmt.Println("UPDATE CALLED")
+	query := `UPDATE recordings ` +
+		`SET title = @title, audio_location = @audio_location, ` +
+		`artwork_location = @artwork_location, date_uploaded = @date_uploaded, ` +
+		`recording_date = @recording_date, location_id = @location_id, duration = @duration, ` +
+		`format = @format, description = @description, equipment = @equipment, file_size = @file_size, ` +
+		`channels = @channels, license = @license ` +
+		`WHERE id = @id`
+	args := pgx.NamedArgs{
+		"title":          recording.Title,
+		"audio_location": recording.AudioLocation,
+		"date_uploaded":  recording.DateUploaded,
+		"recording_date": recording.RecordingDate,
+		"location_id":    recording.LocationID,
+		"duration":       recording.Duration,
+		"format":         recording.Format,
+		"description":    recording.Description,
+		"equipment":      recording.Equipment,
+		"file_size":      recording.Size,
+		"channels":       recording.Channels,
+		"license":        recording.License,
+		"id":             recording.ID,
+	}
+	_, err := r.conn.Exec(ctx, query, args)
+	if err != nil {
+		return fmt.Errorf("unable to insert row: %w", err)
+	}
 	return nil
 }
 
