a3d499ef64690154f726639277c15cc061a63aae
George-Paul-1 <george.paul5236@gmail.com>
2025-01-15 10:54:13 +0000

    added test for list Items route

--- routes/routes_test.go
+++ routes/routes_test.go
@@ -89,3 +89,55 @@ func TestRecordingsGetByIDRoute(t *testing.T) {
   "License": "Creative Commons"
 }`, w.Body.String())
 }
+
+func TestListGETRoute(t *testing.T) {
+	router := gin.Default()
+
+	mockResponse := []entities.Recording{
+		{
+			ID:              1,
+			Title:           "Test Title",
+			AudioLocation:   "test/audio/location.mp3",
+			ArtworkLocation: nil,
+			DateUploaded:    nil,
+			RecordingDate:   time.Date(2025, 1, 6, 20, 2, 57, 0, time.UTC),
+			LocationID:      1,
+			Duration:        120,
+			Format:          "mp3",
+			Description:     "test description",
+			Equipment:       "test equipment",
+			Size:            2048,
+			Channels:        "2",
+			License:         "Creative Commons",
+		},
+	}
+	mockService := &mockService{
+		mockListItems: func(limit int, ctx context.Context) ([]entities.Recording, error) {
+			return mockResponse, nil
+		},
+	}
+	h := handlers.RecordingHandler{Service: mockService}
+	DefineRoutes(router, &h)
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/recordings/list/1", nil)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+	assert.JSONEq(t, `[{
+  "ID": 1,
+  "Title": "Test Title",
+  "AudioLocation": "test/audio/location.mp3",
+  "ArtworkLocation": null,
+  "DateUploaded": null,
+  "RecordingDate": "2025-01-06T20:02:57Z",
+  "LocationID": 1,
+  "Duration": 120,
+  "Format": "mp3",
+  "Description": "test description",
+  "Equipment": "test equipment",
+  "Size": 2048,
+  "Channels": "2",
+  "License": "Creative Commons"
+}]`, w.Body.String())
+}
